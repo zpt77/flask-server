c6a314e57b5e1e1b6cfc256680fa73e3f7ce287b
Tomek Zubrzycki <zpt77@wp.pl>
2023-04-04 00:56:49 +0200

    sqlalchemy

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
